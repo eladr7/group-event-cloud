904ef3a5447531799ab8872ecf3efe701dcf1759
sharp7 <eladr7@gmail.com>
2019-07-24 15:49:48 +0300

    yarn install untill adding proxy in client

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+// import MainPage from './components/main-page';
 import * as serviceWorker from './serviceWorker';
 
+// ReactDOM.render(<MainPage />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
+// serviceWorker.unregister();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // client/src/index.js
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+
+// // // If you want your app to work offline and load faster, you can change
+// // // unregister() to register() below. Note this comes with some pitfalls.
+// // // Learn more about service workers: https://bit.ly/CRA-PWA
+// // serviceWorker.unregister();
