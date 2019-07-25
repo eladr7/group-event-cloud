dd1c4920ec7e21450bab928502df07f636b3eb3c
sharp7 <eladr7@gmail.com>
2019-07-25 12:38:24 +0300

    after installing form&validatorjs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,24 @@
 import { configure } from '@storybook/react';
 
+import '../src/style.css';
+
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
+const req = require.context('../src', true, /.story.jsx?$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
+
+
+
+
+// import { configure } from '@storybook/react';
+
+// // automatically import all files ending in *.stories.js
+// const req = require.context('../stories', true, /\.stories\.js$/);
+// function loadStories() {
+//   req.keys().forEach(filename => req(filename));
+// }
+
+// configure(loadStories, module);
