b8ac69ac8ba0b0cdd16ae10f705898b3d7a04f65
sharp7 <eladr7@gmail.com>
2019-08-20 12:23:31 +0300

    small fix for logout component

--- src/components/Header/logout/Logout.story.jsx
+++ src/components/Header/logout/Logout.story.jsx
@@ -5,7 +5,27 @@ import { storiesOf } from '@storybook/react';
 import Logout from './Logout';
 
 
-storiesOf('Footer molecules', module)
+let users = [
+  {
+      id: 1,
+      username: 'test',
+      password: '123'
+  },
+  {
+      id: 2,
+      username: 'test2',
+      password: '456'
+  }
+];
+
+let user = {id: 1,
+  username: 'SukaMan',
+  password: '123'};
+
+storiesOf('Header logout', module)
   .add('Logout ', () => (
-    <Logout />
+    <Logout 
+      user={user}
+      logo="https://res.cloudinary.com/sivadass/image/upload/v1494699523/icons/bare-tree.png"
+    />
   ));
